04fdf2e0214d37221202550b7b263cdb526a3863
Paulina Krzyżostan <Paulina.Krzyzostan@rossmann.com.pl>
2022-03-15 23:05:01 +0100

    Add button and input

---
+++ src/components/RInput/RInput.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { MDCTextField } from "@material/textfield";
+
+onMounted(() => {
+  new MDCTextField(document.querySelector(".mdc-text-field") as Element);
+});
+
+const props = defineProps({
+  label: { type: String },
+  value: { type: String },
+});
+</script>
+
+<template>
+  <label class="mdc-text-field mdc-text-field--filled">
+    <span class="mdc-text-field__ripple"></span>
+    <input
+      type="text"
+      class="mdc-text-field__input"
+      aria-labelledby="my-label"
+    />
+    <span class="mdc-floating-label" id="my-label">{{ props.label }}</span>
+    <span class="mdc-line-ripple">{{ props.value }}</span>
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "r-input",
+});
+</script>
+
+<style lang="scss">
+@import "@material/textfield/mdc-text-field";
+</style>
