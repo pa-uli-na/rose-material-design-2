d7c045e8da76be1d4b115bc92b2573b98fdfda88
Paulina Krzyżostan <Paulina.Krzyzostan@rossmann.com.pl>
2022-03-21 18:47:11 +0100

    Zmiany

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,5 +1,9 @@
 <template>
-  <label class="mdc-login mdc-text-field mdc-text-field--filled">
+  <!-- MATERIAL DESIGN 2 -->
+  <label
+    class="mdc-login mdc-text-field mdc-text-field--filled"
+    style="width: 100%; margin-top: 10px; margin-bottom: 10px"
+  >
     <span class="mdc-text-field__ripple"></span>
     <span class="mdc-floating-label" id="my-label-id">Login</span>
     <input
@@ -11,9 +15,12 @@
   </label>
   <br />
 
-  <label class="mdc-password mdc-text-field mdc-text-field--filled">
+  <label
+    class="mdc-password mdc-text-field mdc-text-field--filled"
+    style="width: 100%; margin-top: 10px; margin-bottom: 10px"
+  >
     <span class="mdc-text-field__ripple"></span>
-    <span class="mdc-floating-label" id="my-label-id">Password</span>
+    <span class="mdc-floating-label" id="my-label-id">Hasło</span>
     <input
       class="mdc-text-field__input"
       type="password"
@@ -23,13 +30,41 @@
   </label>
   <br />
 
-  <div class="mdc-touch-target-wrapper">
-    <button class="mdc-button mdc-button--touch mdc-button--raised">
+  <div
+    class="mdc-touch-target-wrapper"
+    style="width: 100%; margin-top: 10px; margin-bottom: 10px"
+  >
+    <button
+      class="mdc-button mdc-button--touch mdc-button--raised"
+      style="width: 100%"
+    >
       <span class="mdc-button__ripple"></span>
       <span class="mdc-button__touch"></span>
       <span class="mdc-button__label">Zaloguj</span>
     </button>
   </div>
+  <br />
+
+  <template v-for="role in roles" :key="role.value">
+    <div class="mdc-form-field">
+      <div class="mdc-radio">
+        <input
+          class="mdc-radio__native-control"
+          type="radio"
+          :id="`radio-{role.value}`"
+          name="radios"
+          checked
+        />
+        <div class="mdc-radio__background">
+          <div class="mdc-radio__outer-circle"></div>
+          <div class="mdc-radio__inner-circle"></div>
+        </div>
+        <div class="mdc-radio__ripple"></div>
+      </div>
+      <label for="radio-1">{{ role.label }}</label>
+    </div>
+    <br />
+  </template>
 </template>
 
 <script lang="ts" setup>
@@ -41,10 +76,13 @@ onMounted(() => {
   new MDCTextField(document.querySelector(".mdc-password") as HTMLInputElement);
 });
 
-const log = (arg: unknown) => console.log(arg);
-onMounted(() => {
-  log("login");
-});
+const roles = [
+  { label: "Pracownik sklepu", value: "store_employee" },
+  { label: "Pracownik ochrony", value: "security_employee" },
+  { label: "Pracownik uczący się", value: "newbee_employee" },
+  { label: "Pracownik centrala", value: "central_employee" },
+  { label: "Pracownik zewnętrzny", value: "external_employee" },
+];
 </script>
 
 <script lang="ts">
